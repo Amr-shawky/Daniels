f922ae04ca8694d226d8cc6865308f3ed46d6522
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-05-13 13:04:05 +0300

    Refactor navbar styles for improved layout and responsiveness

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,27 @@
 window.addEventListener("scroll", function () {
+    const homeSection = document.getElementById('home');
     let navbar = document.querySelector(".navbar");
     let navlinks = document.querySelectorAll(".nav-link");
-    
-    if (window.scrollY > 0) {
+    let logo = document.querySelector(".logo");
+    let container = document.querySelector(".navbar .container");
+    const endOfHome = homeSection.offsetTop + homeSection.offsetHeight;
+
+    if (window.scrollY > endOfHome) {
         navbar.classList.add("scrolled");
         navlinks.forEach(link => {
             link.classList.add("scrolled-link");
         });
+        logo.classList.add("scrolled-logo");
+        if (container) {
+            container.classList.add("scrolled-container");
+        }
     } else {
         navbar.classList.remove("scrolled");
         navlinks.forEach(link => {
             link.classList.remove("scrolled-link");
         });
+        if (container) {
+            container.classList.remove("scrolled-container");
+        }
     }
 });
